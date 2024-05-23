3e6ea5077907001ab9cca6389b879fc4467f6b9d
manas <manasrai1234567@gmail.com>
2024-05-23 15:41:19 +0530

    Used Jackson

--- src/main/java/com/RabbitMQProject/configuration/ConfigClass.java
+++ src/main/java/com/RabbitMQProject/configuration/ConfigClass.java
@@ -1,6 +1,9 @@
 package com.RabbitMQProject.configuration;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -52,6 +55,18 @@ public class ConfigClass {
         return BindingBuilder.bind(myReplyQueue()).
                 to(exchange()).
                 with(reply_routing_key);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter producerJackson2MessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory) {
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(producerJackson2MessageConverter());
+        return rabbitTemplate;
     }
 
 }
